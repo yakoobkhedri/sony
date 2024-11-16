5a667e9184d6fbd055180fd34d4f5a3cd9bb9e1e
yakoobkhedri <yakkobkhedrii@gmail.com>
2024-11-16 21:18:19 +0330

    commit message

--- src/js/script.js
+++ src/js/script.js
@@ -220,6 +220,24 @@ orderTabs.forEach((item) => {
   })
 })
 
+// input number
+
+let increment = Array.from(document.getElementsByClassName('increment'));
+let decrement = Array.from(document.getElementsByClassName('decrement'));
+
+increment.forEach((item)=>{
+  item.addEventListener('click', function () {
+    item.previousElementSibling.value ++;
+  })
+});
+decrement.forEach((item)=>{
+  item.addEventListener('click', function () {
+    if (item.nextElementSibling.value > 1) {
+      item.nextElementSibling.value --;
+    }
+  })
+});
+
 // acordion
 
 let acordionBtn = Array.from(document.getElementsByClassName('acordionBtn'));
