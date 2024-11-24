6ef01beb218b36442d3edcffec52eeb950023ef1
yakoobkhedri <yakkobkhedrii@gmail.com>
2024-11-24 15:32:02 +0330

    commit message

--- src/js/script.js
+++ src/js/script.js
@@ -341,3 +341,13 @@ acordionBtn2.forEach((item) => {
     item.querySelector('img').classList.toggle('active');
   })
 })
+
+// 
+
+window.addEventListener('load',function () {
+  const table= document.querySelector('.editor-section').querySelector('table');
+  const newDiv = document.createElement("div");
+  table.parentNode.insertBefore(newDiv,table);
+  newDiv.appendChild(table);
+  newDiv.classList.add('overflow-x-auto');
+})
